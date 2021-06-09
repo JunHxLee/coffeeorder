c9b1aaec10f84d9e85ef4d03cbb0453d42640c45
root <soulfactor@gmail.com>
2021-06-09 08:40:34 +0000

    cloud-work

--- order/src/main/java/coffeeorder/OrderController.java
+++ order/src/main/java/coffeeorder/OrderController.java
@@ -1,12 +1,20 @@
 package coffeeorder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import jdk.internal.misc.Unsafe;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,12 +28,16 @@ public class OrderController {
     public void outofstockCoffee(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("##### /order/outofstockCoffee  called #####");
 
+        String configMap = System.getenv().getOrDefault("GREETING_PREFIX", "Hi");
+
+
         Order order = new Order();
         order.setId(Long.valueOf(request.getParameter("orderId")));
         order.setCoffeeName(request.getParameter("coffeeName"));
-        order.setOrderStatus("Coffee Canceled : Out of stock!!!");
+        order.setOrderStatus("Coffee Canceled : Out of stock!!!  **configMap test = "+configMap );
         orderRepository.save(order);
 
     }
+
 
 }
--- order/src/main/java/coffeeorder/OrderRepository.java
+++ order/src/main/java/coffeeorder/OrderRepository.java
@@ -1,10 +1,12 @@
 package coffeeorder;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel="orders", path="orders")
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+//public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+public interface OrderRepository extends CrudRepository<Order, Long>{    
 
 
 }
